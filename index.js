92b911d5a870632a07e343bd26e2f4fe88bedf2a
Alec Lownes <alec.lownes@gmail.com>
2017-01-09 17:03:37 -0500

    Make mandelbrot be able to run in a gh-pages branch

--- index.js
+++ index.js
@@ -1,38 +1,16 @@
 'use strict';
 
 const httpLib = require('http');
-const path = require('path');
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const app = express();
 const http = httpLib.Server(app); // eslint-disable-line new-cap
 
-// parse application/json
-app.use(bodyParser.json({limit: '500mb'}));
-app.use(bodyParser.urlencoded({extended: false}));
-
 // Deliver the public folder statically
-app.use(express.static('public'));
-
-let apiOptions = {
-	app: app
-};
-
-// Load the api versions
-require('./api/v1')(apiOptions);
+app.use(express.static('.'));
 
 // This tells the server to listen
-var port = process.env.PORT;
+var port = 4000;
 http.listen(port, function () {
-	console.log('Example app listening on port ' + port + '!');
-});
-
-/*
-* This tells the server to always serve index.html no matter what,
-* excluding the previously defined api routes. This is so we can use
-* react-router's browserHistory feature.
-*/
-app.get('*', function (req, res) {
-	res.sendFile(path.join(__dirname, '/public/html/index.html'));
+	console.log('Example app at http://localhost:' + port);
 });
